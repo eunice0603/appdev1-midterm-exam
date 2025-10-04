1e4a76564b810bfa999824d1ad8ee90491ed8a68
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-10-04 08:57:15 +0800

    Added the <head> in index.html and converted the preloader to JSX

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {  
+  return (
+    <>
+      {/* ***** Preloader Start ***** */}
+      <div id="js-preloader" className="js-preloader">
+        <div className="preloader-inner">
+          <span className="dot" />
+          <div className="dots">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+      </div>
+      {/* ***** Preloader End ***** */}
+    </>
+  )
+}
+
+export default App
